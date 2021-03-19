6f4bccb6f9fb66839eece0fd8447dce50e0f5e99
Evgeny Svirin <esvirin@mail.com>
2021-03-19 01:51:26 +0100

    just neeed to fix scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,73 @@
-document.addEventListener('scroll', function(event){
-  console.log(window.scrollY) 
+const floatFooter = document.querySelector('.float__footer')
+const inputRange = document.querySelector('#input__range')
+const rangeImage = document.querySelector('.range__input img')
+const rangeFill = document.querySelector('.range__input .fill')
+const sectionThree = document.querySelector('.section-three')
+const breakPoints = [0, 768, 1536]
+
+let currentPage = 0
+let touchStart = 0
+let touchMove = 0
+let touchModule = 0 
+
+
+document.addEventListener('scroll', function(){
+  let position = window.scrollY
+  position > 800 ? floatFooter.style.display = 'none' : floatFooter.style.display = 'flex'
+  
+  if(position >= 1530){
+  currentPage = 2 
+}else if(position < 767){
+  currentPage = 0
+}else {
+  currentPage = 1}
+  
 })
+
+
+document.addEventListener('touchstart', start =>{
+  touchStart = start.touches[0].clientY
+})
+
+document.addEventListener('touchmove', move=>{
+  move.preventDefault()
+  touchMove = move.touches[0].clientY
+  touchModule = touchStart - touchMove
+},{ passive: false })
+
+document.addEventListener('touchend', movePage())
+
+function movePage(){
+  if(touchModule > 200 && currentPage < 2) {
+    window.scrollTo(0, breakPoints[currentPage + 1])
+  }
+  if(touchModule < -200 && currentPage > 0) {
+    window.scrollTo(0, breakPoints[currentPage - 1])
+  }
+}
+
+// logic of input-range
+inputRange.addEventListener('change',function(ev){
+
+  switch(+ev.target.value){
+    case 0 :
+      rangeFill.style.width = '0%'
+      rangeImage.style.marginLeft = 'inherit'
+      sectionThree.style.backgroundPosition = 'left'
+    break;
+
+    case 1 : 
+      rangeFill.style.width = '50%'
+      rangeImage.style.marginLeft = '50%'
+      sectionThree.style.backgroundPosition = 'center'
+    break;
+
+    case 2 :
+      rangeFill.style.width = '100%'
+      rangeImage.style.marginLeft = '100%'
+      sectionThree.style.backgroundPosition = 'right'
+    break;
+  }
+})
+
+
